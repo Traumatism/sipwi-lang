7f686bfcc3986a5bf9836e39476ca2ecd074915a
toast <58362380+Traumatism@users.noreply.github.com>
2022-10-28 09:54:23 +0200

    Commit

---
+++ src/standard/random.rs
@@ -0,0 +1,37 @@
+use crate::lexing::token::Token;
+use crate::parsing::structs::{StdFuncResult, Variable};
+use crate::sipwi::Sipwi;
+
+pub fn std_randint(env: &&mut Sipwi, token: Token) -> StdFuncResult {
+    let mut start_end: Vec<isize> = Vec::new();
+
+    if let Token::List(lst_content) = token {
+        if lst_content.len() != 2 {
+            panic!()
+        }
+
+        for lst in lst_content {
+            if lst.len() != 1 {
+                panic!()
+            }
+
+            for element in lst {
+                match element {
+                    Token::Number(n) => start_end.push(n),
+                    Token::Identifier(identifier) => match env.get_variable(&identifier).clone() {
+                        Some(&Variable::Number(value)) => start_end.push(value),
+                        _ => panic!(),
+                    },
+                    _ => panic!(),
+                }
+            }
+        }
+    }
+
+    StdFuncResult::new(Token::List(std::vec::from_elem(
+        (start_end[0].clone()..start_end[1].clone())
+            .map(|n| Token::Number(n))
+            .collect(),
+        1,
+    )))
+}
